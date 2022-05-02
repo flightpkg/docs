cae283f80cbb6ad5cefe0432d130ee930bfb9547
DevComp <hi@devcomp.tk>
2022-05-02 11:05:13 +0000

    🚚 Add quickrelease link route

---
+++ src/pages/Get/index.js
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+ import { Redirect } from '@docusaurus/router';
+ import React, { useState, useEffect } from "react";
+
+ import Layout from "@theme/Layout";
+ 
+ // import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+ import Link from "@docusaurus/Link";
+ import useBaseUrl from "@docusaurus/useBaseUrl";
+ 
+ import versions from "../../../versions.json";
+ import api from "../../services/api";
+ 
+ function Get() {
+   // const context = useDocusaurusContext();
+   // const { siteConfig = {} } = context;
+   const [latestVersion] = versions;
+   const pastVersions = versions.filter(version => version !== latestVersion);
+   // const repoUrl = `https://github.com/${siteConfig.organizationName}/${siteConfig.projectName}`;
+   const [version, setVersion] = useState("v0.0.5");
+
+   useEffect(() => {
+     api.get("/repos/flightpkg/flight/tags").then(response => {
+       if (response.status === 200 && response.data.length > 0) {
+         const [{ name }] = response.data;
+         setVersion(name);
+       }
+     });
+   }, []);
+
+   useEffect(() => {
+    window.location.href = `https://github.com/flightpkg/flight/releases/download/${version}/install.sh`;
+  }, [])
+   return (
+    "Redirecting to latest release"
+ );
+}
+ 
+ export default Get;
